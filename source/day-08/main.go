5cc9b534012e2fe2f2b2e69ba78f305b2f79fd27
LenaBullens <BullensLena@gmail.com>
2021-12-08 23:40:37 +0100

    day 8 part 1

--- source/day-08/main.go
+++ source/day-08/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -13,7 +14,23 @@ type display struct {
 }
 
 func main() {
-	readInput("input-08-example.txt")
+	fmt.Println(part1(readInput("input-08.txt")))
+}
+
+func part1(displays []display) int {
+	var uniqueDigitCount int
+	for i := 0; i < len(displays); i++ {
+		for j := 0; j < len(displays[i].outputValues); j++ {
+			//len is only appropriate to use to determine number of characters in a string if
+			//the string consists solely of byte-sized characters. *chomp* This is luckily the case here.
+			length := len(displays[i].outputValues[j])
+			//Digits with unique amount of segements are 1 (2 seg), 4 (4 seg), 7 (3 seg) and 8 (7 seg).
+			if length == 2 || length == 3 || length == 4 || length == 7 {
+				uniqueDigitCount = uniqueDigitCount + 1
+			}
+		}
+	}
+	return uniqueDigitCount
 }
 
 func readInput(path string) []display {
