b1e3efee682003b5679572dbffde16d9d65fdead
LenaBullens <BullensLena@gmail.com>
2021-12-19 13:15:02 +0100

    day 16 part 2 + part 1 rewrite

--- source/day-16/main.go
+++ source/day-16/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -29,169 +30,121 @@ var hexToBinMap = map[string]string{
 }
 
 func main() {
-	part1()
-}
-
-func part1() {
 	hex := readInput("input-16.txt")
 	bin := hexToBin(hex)
-	totalVersion := parsePacket(bin)
+	totalVersion, totalValue, _ := parsePacket(bin)
 	fmt.Println("==========")
 	fmt.Printf("Total version: %d\n", totalVersion)
+	fmt.Printf("Total value: %d\n", totalValue)
 }
 
-func parsePacket(bin string) int {
-	var totalVersion int
-
-	cursor := 0
-
-	//First 6 bits
-	packetVersion, packetTypeId := parseHeader(bin[cursor : cursor+6])
-	cursor = cursor + 6
-
-	totalVersion = totalVersion + packetVersion
-
-	if packetTypeId != 4 {
-		//Parse length stuff
-		lengthTypeID := bin[cursor : cursor+1]
-		cursor = cursor + 1
-		var length int
-		if lengthTypeID == "0" {
-			lengthBin := bin[cursor : cursor+15]
-			length = binToInt(lengthBin)
-			cursor = cursor + 15
-			totalVersion = totalVersion + parseSubPacketsMode0(bin[cursor:cursor+length], length)
-		} else {
-			lengthBin := bin[cursor : cursor+11]
-			length = binToInt(lengthBin)
-			cursor = cursor + 11
-			addedVersion, _ := parseSubPacketsMode1(bin[cursor:], length)
-			totalVersion = totalVersion + addedVersion
-		}
+func parsePacket(bin string) (int, int, int) {
+	//Minimal length of a packet is 11; a literal packet with 4 bit of literal data.
+	if len(bin) < 11 {
+		return 0, 0, 0
 	}
 
-	return totalVersion
-}
+	//Amount of bits parsed in this package.
+	bitsParsed := 0
+	packetValue := 0
 
-func parseHeader(bin string) (int, int) {
-	cursor := 0
+	//Version
+	packetVersion := binToInt(bin[bitsParsed : bitsParsed+3])
+	bitsParsed = bitsParsed + 3
 
-	//First 3 bits
-	packetVersionBin := bin[cursor : cursor+3]
-	packetVersion := binToInt(packetVersionBin)
-	cursor = cursor + 3
-	fmt.Printf("Packet version: %d\n", packetVersion)
+	//Type
+	packetType := binToInt(bin[bitsParsed : bitsParsed+3])
+	bitsParsed = bitsParsed + 3
 
-	//Next 3 bits
-	packetTypeIDBin := bin[cursor : cursor+3]
-	packetTypeID := binToInt(packetTypeIDBin)
-	fmt.Printf("Packet ID: %d\n", packetTypeID)
-	return packetVersion, packetTypeID
-}
-
-func parseSubPacketsMode0(bin string, totalLength int) int {
-	var totalVersion int
-
-	cursor := 0
-	done := false
-
-	for !done {
-		//First 6 bits
-		packetVersion, packetTypeId := parseHeader(bin[cursor : cursor+6])
-		cursor = cursor + 6
-
-		totalVersion = totalVersion + packetVersion
-
-		if packetTypeId != 4 {
-			//Parse length stuff
-			lengthTypeID := bin[cursor : cursor+1]
-			cursor = cursor + 1
-			var length int
-			if lengthTypeID == "0" {
-				lengthBin := bin[cursor : cursor+15]
-				length = binToInt(lengthBin)
-				cursor = cursor + 15
-				totalVersion = totalVersion + parseSubPacketsMode0(bin[cursor:cursor+length], length)
-				cursor = cursor + length
-			} else {
-				lengthBin := bin[cursor : cursor+11]
-				length = binToInt(lengthBin)
-				cursor = cursor + 11
-				addedVersion, cursorShift := parseSubPacketsMode1(bin[cursor:], length)
-				totalVersion = totalVersion + addedVersion
-				cursor = cursor + cursorShift
+	if packetType == 4 {
+		doneLiteral := false
+		var valueInBin string
+		for !doneLiteral {
+			fiveBits := bin[bitsParsed : bitsParsed+5]
+			oneBit := fiveBits[:1]
+			valueInBin = valueInBin + fiveBits[1:]
+			bitsParsed = bitsParsed + 5
+			if oneBit == "0" {
+				doneLiteral = true
 			}
+		}
+		packetValue = binToInt(valueInBin)
+	} else {
+		//Check type of length
+		lengthType := bin[bitsParsed : bitsParsed+1]
+		bitsParsed = bitsParsed + 1
+		if lengthType == "0" {
+			length := binToInt(bin[bitsParsed : bitsParsed+15])
+			bitsParsed = bitsParsed + 15
+			subBitsParsed := 0
+			var values []int
+			for subBitsParsed < length {
+				subVersion, subValue, subBits := parsePacket(bin[bitsParsed+subBitsParsed:])
+				packetVersion = packetVersion + subVersion
+				values = append(values, subValue)
+				subBitsParsed = subBitsParsed + subBits
+			}
+			bitsParsed = bitsParsed + subBitsParsed
+			packetValue = calculateValue(values, packetType)
 		} else {
-			doneLiteral := false
-			for !doneLiteral {
-				fiveBits := bin[cursor : cursor+5]
-				oneBit := fiveBits[:1]
-				cursor = cursor + 5
-				if oneBit == "0" {
-					doneLiteral = true
-				}
+			nbOfPackets := binToInt(bin[bitsParsed : bitsParsed+11])
+			bitsParsed = bitsParsed + 11
+			subBitsParsed := 0
+			var values []int
+			for i := 0; i < nbOfPackets; i++ {
+				subVersion, subValue, subBits := parsePacket(bin[bitsParsed+subBitsParsed:])
+				packetVersion = packetVersion + subVersion
+				values = append(values, subValue)
+				subBitsParsed = subBitsParsed + subBits
 			}
-		}
-		if cursor >= totalLength {
-			done = true
-		}
-
-	}
-
-	return totalVersion
-}
-
-func parseSubPacketsMode1(bin string, totalLength int) (int, int) {
-	var totalVersion int
-
-	cursor := 0
-	nbPacketsParsed := 0
-	done := false
-
-	for !done {
-		//First 6 bits
-		packetVersion, packetTypeId := parseHeader(bin[cursor : cursor+6])
-		cursor = cursor + 6
-		nbPacketsParsed = nbPacketsParsed + 1
-
-		totalVersion = totalVersion + packetVersion
-
-		if packetTypeId != 4 {
-			//Parse length stuff
-			lengthTypeID := bin[cursor : cursor+1]
-			cursor = cursor + 1
-			var length int
-			if lengthTypeID == "0" {
-				lengthBin := bin[cursor : cursor+15]
-				length = binToInt(lengthBin)
-				cursor = cursor + 15
-				totalVersion = totalVersion + parseSubPacketsMode0(bin[cursor:cursor+length], length)
-				cursor = cursor + length
-			} else {
-				lengthBin := bin[cursor : cursor+11]
-				length = binToInt(lengthBin)
-				cursor = cursor + 11
-				addedVersion, cursorShift := parseSubPacketsMode1(bin[cursor:], length)
-				totalVersion = totalVersion + addedVersion
-				cursor = cursor + cursorShift
-			}
-		} else {
-			doneLiteral := false
-			for !doneLiteral {
-				fiveBits := bin[cursor : cursor+5]
-				oneBit := fiveBits[:1]
-				cursor = cursor + 5
-				if oneBit == "0" {
-					doneLiteral = true
-				}
-			}
-		}
-		if nbPacketsParsed >= totalLength {
-			done = true
+			bitsParsed = bitsParsed + subBitsParsed
+			packetValue = calculateValue(values, packetType)
 		}
 	}
+	return packetVersion, packetValue, bitsParsed
+}
 
-	return totalVersion, cursor
+func calculateValue(values []int, packetType int) int {
+	result := 0
+	if packetType == 0 {
+		for _, v := range values {
+			result = result + v
+		}
+	} else if packetType == 1 {
+		result = 1
+		for _, v := range values {
+			result = result * v
+		}
+	} else if packetType == 2 {
+		min := math.MaxInt32
+		for _, v := range values {
+			if v < min {
+				min = v
+			}
+		}
+		result = min
+	} else if packetType == 3 {
+		max := math.MinInt32
+		for _, v := range values {
+			if v > max {
+				max = v
+			}
+		}
+		result = max
+	} else if packetType == 5 {
+		if values[0] > values[1] {
+			result = 1
+		}
+	} else if packetType == 6 {
+		if values[0] < values[1] {
+			result = 1
+		}
+	} else if packetType == 7 {
+		if values[0] == values[1] {
+			result = 1
+		}
+	}
+	return result
 }
 
 func hexToBin(hex string) string {
